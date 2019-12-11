326e668d19e0ddfb9889e7ed95da4d10447561ca
Haard Panchal <panchalhaard@gmail.com>
2019-12-11 14:53:42 -0600

    Fixing errors

--- webgl/js/voronoi2d.js
+++ webgl/js/voronoi2d.js
@@ -1,9 +1,11 @@
+import * as THREE from 'three';
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-var geometry = new THREE.ConeGeometry(1, 1, 1);
+var geometry = new THREE.ConeGeometry(5, 20, 32);
 var material = new THREE.MeshBasicMaterial({
     color: 0x00ff00
 });
